1225c8afc3eff4f0d85f3b8e0536c24342764beb
jeppe jørgensen <jeppegroenj@gmail.com>
2022-08-20 15:19:58 +0200

    updatet app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = "https://etilbudsavis.dk/"
+URL = "https://etilbudsavis.dk/soeg/%C3%A6g"
 page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="main")
+offer_elements = results.find_all("li", class_="UniversalCardList__Item-sc-4v1xeg-4 klLrVZ")
 
-print(page.text)
+
+
+for offer_element in offer_elements:
+    #header_element = offer_element.find("header")
+    #price_element = offer_element.find(class_="Stack__Item-sc-u1ffod-2 Stack___StyledItem-sc-u1ffod-3 cWwVbp kgTTno")
+    #location_element = offer_element.find("p", class_="location")
+    print(offer_element.find(class_="BusinessLabel__BusinessLabelDiv-sc-iksnba-0 ijMGWk").text.strip())
+    print(offer_element.find("header").text.strip())
+    print(offer_element.find(class_="UniversalCardList___StyledStack-sc-4v1xeg-7 dYBUkI").text.strip())
+    print(offer_element.find("meta", itemprop="priceValidUntil"))
+    print(offer_element.find(itemprop="validFrom")[0])
+    print(str(offer_element.find("meta", itemprop="validThrough").unwrap()))
+    #print(location_element.text.strip())
+    print()
