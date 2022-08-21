4c5b442d0c8bed92fa15f537e8dc07ff0ba20986
jeppe jørgensen <jeppegroenj@gmail.com>
2022-08-21 17:58:18 +0200

    Fixed printing dates

--- app.py
+++ app.py
@@ -10,14 +10,12 @@ offer_elements = results.find_all("li", class_="UniversalCardList__Item-sc-4v1xe
 
 
 for offer_element in offer_elements:
-    #header_element = offer_element.find("header")
-    #price_element = offer_element.find(class_="Stack__Item-sc-u1ffod-2 Stack___StyledItem-sc-u1ffod-3 cWwVbp kgTTno")
-    #location_element = offer_element.find("p", class_="location")
     print(offer_element.find(class_="BusinessLabel__BusinessLabelDiv-sc-iksnba-0 ijMGWk").text.strip())
     print(offer_element.find("header").text.strip())
     print(offer_element.find(class_="UniversalCardList___StyledStack-sc-4v1xeg-7 dYBUkI").text.strip())
-    print(offer_element.find("meta", itemprop="priceValidUntil"))
-    print(offer_element.find(itemprop="validFrom")[0])
-    print(str(offer_element.find("meta", itemprop="validThrough").unwrap()))
-    #print(location_element.text.strip())
+    print(offer_element.find(itemprop="priceValidUntil").get("content"))
+    print(offer_element.find(itemprop="validFrom").get("content"))
+    print(offer_element.find(itemprop="validThrough").get("content"))
+    #print(offer_element.find(content, itemprop="validThrough"))
+    #print(offer_element.find('meta', attrs={'itemprop':'validFrom'}).text)
     print()
