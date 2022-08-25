33b7f420b8fe86ce48b71d5a9293141881fa88b4
jeppe jørgensen <jeppegroenj@gmail.com>
2022-08-25 11:27:46 +0200

    Added date if statement

--- app.py
+++ app.py
@@ -4,36 +4,24 @@ from datetime import datetime
 from datetime import date
 import pprint
 
-
 URL = "https://etilbudsavis.dk/soeg/%C3%A6g"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="main")
-offer_elements = results.find_all("li", class_="UniversalCardList__Item-sc-4v1xeg-4 klLrVZ")
+offer_elements = results.find_all(
+    "li", class_="UniversalCardList__Item-sc-4v1xeg-4 klLrVZ")
 
-stores = ["Rema 1000", "Netto"]
-product_list  = []
-today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(today)
-print(type(today))
+stores = ["Rema 1000", "Netto", "Føtex"]
+product_list = []
+today = datetime.today()
 
 for offer_element in offer_elements:
-    case = {'store': offer_element.find(class_="BusinessLabel__BusinessLabelDiv-sc-iksnba-0 ijMGWk").text.strip(), 'name': offer_element.find("header").text.strip(), 'price':offer_element.find(class_="UniversalCardList___StyledStack-sc-4v1xeg-7 dYBUkI").text.strip(), 'fromDate': offer_element.find(itemprop="validFrom").get("content"), 'toDate': offer_element.find(itemprop="priceValidUntil").get("content")}
+    case = {'store': offer_element.find(class_="BusinessLabel__BusinessLabelDiv-sc-iksnba-0 ijMGWk").text.strip(), 'name': offer_element.find("header").text.strip(), 'price': offer_element.find(
+        class_="UniversalCardList___StyledStack-sc-4v1xeg-7 dYBUkI").text.strip(), 'fromDate': offer_element.find(itemprop="validFrom").get("content")[0:10], 'toDate': offer_element.find(itemprop="priceValidUntil").get("content")[0:10]}
+    from_date = datetime. strptime(case['fromDate'], '%Y-%m-%d')
+    to_date = datetime. strptime(case['toDate'], '%Y-%m-%d')
     if case['store'] in stores:
-        #if today <= datetime(case['fromDate']) & today >= datetime(case['toDate']):
-        #if today.format(datetime) <= datetime(datetime.fromisoformat(case['fromDate'][:-15])):
-        #date_time_obj = datetime. strptime(date_time_str, '%d/%m/%y %H:%M:%S')
+        if today >= from_date and today <= to_date:
             product_list.append(case)
 
-
 pprint.pprint(product_list)
-
-""" for offer_element in offer_elements:
-    print(offer_element.find(class_="BusinessLabel__BusinessLabelDiv-sc-iksnba-0 ijMGWk").text.strip())
-    print(offer_element.find("header").text.strip())
-    print(offer_element.find(class_="UniversalCardList___StyledStack-sc-4v1xeg-7 dYBUkI").text.strip())
-    print(offer_element.find(itemprop="priceValidUntil").get("content"))
-    print(offer_element.find(itemprop="validFrom").get("content"))
-    print(datetime.fromisoformat(offer_element.find(itemprop="validThrough").get("content")[:-15]))
-    datetime.fromisoformat(case['fromDate'][:-15])
-    print()  """
