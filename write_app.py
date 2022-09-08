e57f2e3758504efa292f582ef19e16b23ca56c56
jeppe jørgensen <jeppegroenj@gmail.com>
2022-09-08 20:56:45 +0200

    Changed to write json

--- write_app.py
+++ write_app.py
@@ -1,8 +1,19 @@
 from app import groceries
 from app import dba
 import pprint
+import json
 
 def write_list():
+    grocery_list = groceries()
+    dba_list = dba()
+    with open('offers.txt', 'w') as reader:
+        reader.write((json.dumps(grocery_list, indent=4)) + "\n")
+        reader.write((json.dumps(dba_list, indent=4)) + "\n") 
+    reader.close()
+
+write_list()
+
+""" def write_list():
     grocery_list = groceries()
     dba_list = dba()
     with open('offers.txt', 'w') as reader:
@@ -12,4 +23,4 @@ def write_list():
             reader.write((str(item)) + "\n")
     reader.close()
 
-write_list()
+write_list() """
